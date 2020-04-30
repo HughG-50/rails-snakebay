706e9123c912b14a6e6dfff75a77d22d9d10d14a
HughG-50 <hugh.greethead@gmail.com>
2020-04-30 16:18:20 +1000

    Updated webhook

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,16 @@ class PaymentsController < ApplicationController
     end
 
     def webhook 
+        payment_id= params[:data][:object][:payment_intent]
+        payment = Stripe::PaymentIntent.retrieve(payment_id)
+        listing_id = payment.metadata.listing_id
+        user_id = payment.metadata.user_id
+
+        p "listing id " + listing_id
+        p "user id " + user_id
         p params 
+
+        status 200
     end
 
 end
